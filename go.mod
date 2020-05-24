83d4e51e017359c564a8389cc8628a61d3deecbd
arunavasahoo <arunava_sahoo@yahoo.co.in>
2020-05-24 15:21:12 +0000

    My Sample MOD code

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/arunavasahoo/samplemod
+
+go 1.13
+
+require github.com/arunavasahoo/mymodproj v0.0.0-20200524114849-fc1aef78e93e // indirect
---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+        "fmt"
+        "github.com/arunavasahoo/mymodproj"
+)
+
+func main() {
+        fmt.Println("Hello, playground")
+
+fmt.Println(mymodproj.ReturnString(" Arunava Sahoo"))
+}
